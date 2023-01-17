624f3a309f9620e77877613e4531e10da0a83e5e
Corey Shupe <coreyshupe101@gmail.com>
2023-01-16 20:16:52 -0500

    Remove bad debug

--- src/client.rs
+++ src/client.rs
@@ -152,7 +152,6 @@ impl<A: AsyncWrite + Unpin + Send + Sync> McPacketWriter for A {
             buffer.write_var_int(size).await?;
             P::encode(&packet, &mut (), &mut buffer).await?;
             let mut buffer = buffer.into_inner();
-            println!("Encoding packet {:?}", buffer);
             if let Some(cipher) = cipher {
                 drax::transport::encryption::AsyncStreamCipher::encrypt(cipher, &mut buffer);
             }
